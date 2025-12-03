840f9354f99b2bde4bbc27e8cb5d0a888edaa43b
bobcermak <bohuslavcermak62@gmail.com>
2025-12-03 23:03:39 +0100

    Update AssemblyInfo with new informational version and cache inputs

--- WebGame.Server/obj/Debug/net9.0/WebGame.Server.AssemblyInfo.cs
+++ WebGame.Server/obj/Debug/net9.0/WebGame.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebGame.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fe8521652164016de191a09d2627979c1cda999")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b7fb5ec561aa8278f72b6dab5af13f287b0fb55")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebGame.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebGame.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
