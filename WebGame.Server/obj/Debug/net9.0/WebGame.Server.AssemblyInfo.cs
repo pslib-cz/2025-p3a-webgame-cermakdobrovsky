d9bee86cedfcb9db070c36948052c81b2a02234c
bobcermak <bohuslavcermak62@gmail.com>
2025-12-06 11:27:43 +0100

    Update project paths and configuration files for consistency and user directory changes

--- WebGame.Server/obj/Debug/net9.0/WebGame.Server.AssemblyInfo.cs
+++ WebGame.Server/obj/Debug/net9.0/WebGame.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebGame.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30cb20211515a91fdf0b99f654f4e1f40a5ed74d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2186078c21d51a312be74604774579af1eb7beb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebGame.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebGame.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
