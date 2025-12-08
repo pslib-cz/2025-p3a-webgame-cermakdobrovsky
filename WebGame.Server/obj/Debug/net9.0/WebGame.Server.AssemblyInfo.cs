af716459a5270c2902fd1a71f6cf49ef1d526abf
bobcermak <bohuslavcermak62@gmail.com>
2025-12-08 09:26:00 +0100

    Update AssemblyInfo and project paths for consistency and versioning

--- WebGame.Server/obj/Debug/net9.0/WebGame.Server.AssemblyInfo.cs
+++ WebGame.Server/obj/Debug/net9.0/WebGame.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebGame.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2186078c21d51a312be74604774579af1eb7beb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9bee86cedfcb9db070c36948052c81b2a02234c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebGame.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebGame.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
