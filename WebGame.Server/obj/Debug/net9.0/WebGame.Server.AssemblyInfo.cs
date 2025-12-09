bcd39e75b0836a2097882e93ae1617cae701ac7e
bobcermak <bohuslavcermak62@gmail.com>
2025-12-09 17:06:26 +0100

    Update AssemblyInfo and .gitignore for consistency and versioning

--- WebGame.Server/obj/Debug/net9.0/WebGame.Server.AssemblyInfo.cs
+++ WebGame.Server/obj/Debug/net9.0/WebGame.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebGame.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9bee86cedfcb9db070c36948052c81b2a02234c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38ac22a03b726a2777aafa6f0ee19089cd238f37")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebGame.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebGame.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
