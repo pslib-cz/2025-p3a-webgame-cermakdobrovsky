1c683eac5509c43531b3d3a549f949a039db1b3f
Vít Dobrovský <dobrovsky.vit@gmail.com>
2025-12-15 15:57:46 +0100

    fixes

--- WebGame.Server/obj/Debug/net9.0/WebGame.Server.AssemblyInfo.cs
+++ WebGame.Server/obj/Debug/net9.0/WebGame.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebGame.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ac592115364a614a36d3825210543e10e1379df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28cf987094eb9a1f1b20a967a04b9d9425b86781")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebGame.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebGame.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
