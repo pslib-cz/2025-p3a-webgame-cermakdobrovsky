6ac592115364a614a36d3825210543e10e1379df
Vít Dobrovský <dobrovsky.vit@gmail.com>
2025-12-15 15:10:26 +0100

    git ignore fixes

--- WebGame.Server/obj/Debug/net9.0/WebGame.Server.AssemblyInfo.cs
+++ WebGame.Server/obj/Debug/net9.0/WebGame.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebGame.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5456dc908a59e0524f3938fd0a38b0d2df008b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a99270abc3bef17565fea1498d88296c7f70af99")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebGame.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebGame.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
