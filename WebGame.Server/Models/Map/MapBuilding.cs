30010dd502b03c775f53a49bd8cb33d0320403d0
Vít Dobrovský <dobrovsky.vit@gmail.com>
2025-12-17 14:08:52 +0100

    update

--- WebGame.Server/Models/Map/MapBuilding.cs
+++ WebGame.Server/Models/Map/MapBuilding.cs
@@ -6,13 +6,11 @@ namespace WebGame.Server.Models
     [PrimaryKey(nameof(BuildingId), nameof(MapId))]
     public class MapBuilding
     {
-        public int TopLeftX { get; set; }
-        public int TopLeftY { get; set; }
-
         public int BuildingId { get; set; }
         [ForeignKey(nameof(BuildingId))]
         public Building Building{ get; set; } = null!;
-
         public int MapId { get; set; }
+        public int TopLeftX { get; set; }
+        public int TopLeftY { get; set; }
     }
 }
--- WebGame.Server/Models/Map/MapTile.cs
+++ WebGame.Server/Models/Map/MapTile.cs
@@ -9,8 +9,8 @@ namespace WebGame.Server.Models
         public int TileId { get; set; }
         [ForeignKey(nameof(TileId))]
         public Tile Tile { get; set; } = null!;
+        public int MapId { get; set; }
         public int X { get; set; }
         public int Y { get; set; }
-        public int MapId { get; set; }
     }
 }
