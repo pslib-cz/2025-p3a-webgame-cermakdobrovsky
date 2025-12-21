633ab45e83b6d1716e22098cb73d1499f62d4863
Vít Dobrovský <dobrovsky.vit@gmail.com>
2025-12-21 10:13:33 +0100

    class for handling csv

---
+++ WebGame.Server/Utils/ReadCsv.cs
@@ -0,0 +1,20 @@
+namespace WebGame.Server.Utils
+{
+    public static class CsvParser
+    {
+        public delegate void ValueAction(string value, int x, int y);   
+        public static void RunForEachValue(string path, ValueAction action)
+        {
+            string[] csvLines = System.IO.File.ReadAllLines(path);
+
+            for (int y = 0; y < csvLines.Length; y++)
+            {
+                string[] values = csvLines[y].Split(',');
+                for (int x = 0; x < values.Length; x++)
+                {
+                    action(values[x], x, y);
+                }
+            }
+        }
+    }
+}
