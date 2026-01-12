e2eea97f2947f485709dc54fa7073b080ee792e5
Vít Dobrovský <dobrovsky.vit@gmail.com>
2026-01-12 13:46:47 +0100

    render building

---
+++ WebGame.Server/Migrations/20260112122731_changesBuildingWidth.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebGame.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class changesBuildingWidth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Buildings",
+                keyColumn: "BuildingId",
+                keyValue: 1,
+                columns: new[] { "Height", "Width" },
+                values: new object[] { 6, 9 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Buildings",
+                keyColumn: "BuildingId",
+                keyValue: 1,
+                columns: new[] { "Height", "Width" },
+                values: new object[] { 7, 10 });
+        }
+    }
+}
--- webgame.client/src/Components/Building.tsx
+++ webgame.client/src/Components/Building.tsx
@@ -1,5 +1,6 @@
 import type { MapBuilding } from "../../types/mapModels";
-import { Text } from "react-konva";
+import useImage from "use-image";
+import { Text, Rect } from "react-konva";
 
 type buildingProps = {
   building: MapBuilding;
@@ -7,10 +8,30 @@ type buildingProps = {
 };
 
 const BuildingComponent: React.FC<buildingProps> = ({ building, tileSize }) => {
-  // const [buildingImage] = useImage(building.building.imageUrl);
+  const [buildingImage] = useImage(building.building.imageUrl);
+
+  let scale = 1;
+  let offsetX = 0;
+  let offsetY = 0;
+
+  if (buildingImage) {
+    scale = Math.min(tileSize / buildingImage.width, tileSize / buildingImage.height);
+    offsetX = (tileSize - buildingImage.width * scale) / 2;
+    offsetY = (tileSize - buildingImage.height * scale) / 2;
+  }
 
   return (
     <>
+      <Rect
+        x={building.topLeftX * tileSize}
+        y={building.topLeftY * tileSize}
+        width={tileSize * building.building.width}
+        height={tileSize * building.building.height}
+        fillPatternImage={buildingImage}
+        fillPatternScale={{ x: scale, y: scale }}
+        fillPatternOffset={{ x: -offsetX / scale, y: -offsetY / scale }}
+        fillPatternRepeat="no-repeat"
+      />
       <Text
         text={`x: ${building.topLeftX}\ny: ${building.topLeftY}\n${building.building.width}x${building.building.height}`}
         fontSize={12}
