2c9f7c2d4d6f82c9477df4325e8c04823d0a4aa4
Vít Dobrovský <dobrovsky.vit@gmail.com>
2026-01-12 13:48:58 +0100

    render building

--- webgame.client/src/Components/Building.tsx
+++ webgame.client/src/Components/Building.tsx
@@ -1,6 +1,6 @@
 import type { MapBuilding } from "../../types/mapModels";
 import useImage from "use-image";
-import { Text, Rect } from "react-konva";
+import { Rect } from "react-konva";
 
 type buildingProps = {
   building: MapBuilding;
@@ -11,13 +11,11 @@ const BuildingComponent: React.FC<buildingProps> = ({ building, tileSize }) => {
   const [buildingImage] = useImage(building.building.imageUrl);
 
   let scale = 1;
-  let offsetX = 0;
-  let offsetY = 0;
 
   if (buildingImage) {
-    scale = Math.min(tileSize / buildingImage.width, tileSize / buildingImage.height);
-    offsetX = (tileSize - buildingImage.width * scale) / 2;
-    offsetY = (tileSize - buildingImage.height * scale) / 2;
+    const buildingWidth = tileSize * building.building.width;
+    const buildingHeight = tileSize * building.building.height;
+    scale = Math.min(buildingWidth / buildingImage.width, buildingHeight / buildingImage.height);
   }
 
   return (
@@ -29,23 +27,7 @@ const BuildingComponent: React.FC<buildingProps> = ({ building, tileSize }) => {
         height={tileSize * building.building.height}
         fillPatternImage={buildingImage}
         fillPatternScale={{ x: scale, y: scale }}
-        fillPatternOffset={{ x: -offsetX / scale, y: -offsetY / scale }}
         fillPatternRepeat="no-repeat"
-      />
-      <Text
-        text={`x: ${building.topLeftX}\ny: ${building.topLeftY}\n${building.building.width}x${building.building.height}`}
-        fontSize={12}
-        x={building.topLeftX * tileSize}
-        y={building.topLeftY * tileSize}
-        fill="white"
-        shadowColor="black"
-        shadowBlur={2}
-        padding={5}
-        width={tileSize}
-        align="center"
-        verticalAlign="middle"
-        height={tileSize}
-        listening={false}
       />
     </>
   );
