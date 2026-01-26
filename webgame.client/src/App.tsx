eaefa1c60d6cd5c30ce07323a16cf871899706d6
Vít Dobrovský <dobrovsky.vit@gmail.com>
2026-01-26 15:52:05 +0100

    reset on building placement

--- webgame.client/src/App.tsx
+++ webgame.client/src/App.tsx
@@ -128,6 +128,7 @@ const App = () => {
           onMapClick={(x, y) => {
             if (placingBuilding !== null) {
               addBuilding(placingBuilding.buildingId, x, y);
+              setPlacingBuilding(null);
             }
           }}
         />
