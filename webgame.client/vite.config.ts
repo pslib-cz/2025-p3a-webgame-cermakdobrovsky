a885a2c4b16b889a83f1f771d4bb86de2ccdb57c
bobcermak <bohuslavcermak62@gmail.com>
2026-02-02 15:18:10 +0100

    feat: enhance Vite configuration for development with HTTPS support and certificate generation

--- webgame.client/vite.config.ts
+++ webgame.client/vite.config.ts
@@ -1,17 +1,59 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import plugin from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
+import child_process from 'child_process';
+import { env } from 'process';
 
-const target = process.env.VITE_API_TARGET || "http://localhost:5125";
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: target,
-        changeOrigin: true,
-        secure: false,
-      }
+const isDev = process.argv.includes('--mode') ? process.argv.includes('development') : !process.argv.includes('build');
+let certFilePath = '';
+let keyFilePath = '';
+if (isDev) {
+  const baseFolder =
+    env.APPDATA !== undefined && env.APPDATA !== ''
+      ? `${env.APPDATA}/ASP.NET/https`
+      : `${env.HOME}/.aspnet/https`;
+  const certificateName = "webgame.client";
+  certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+  keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+  if (!fs.existsSync(baseFolder)) {
+    fs.mkdirSync(baseFolder, { recursive: true });
+  }
+  if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+    if (0 !== child_process.spawnSync('dotnet', [
+      'dev-certs',
+      'https',
+      '--export-path',
+      certFilePath,
+      '--format',
+      'Pem',
+      '--no-password',
+    ], { stdio: 'inherit', }).status) {
+      throw new Error("Could not create certificate.");
     }
   }
-});
+}
+const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:5125';
+export default defineConfig({
+  plugins: [plugin()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: isDev ? {
+    proxy: {
+      '^/api': {
+        target,
+        secure: false
+      }
+    },
+    port: parseInt(env.DEV_SERVER_PORT || '5173'),
+    https: {
+      key: fs.readFileSync(keyFilePath),
+      cert: fs.readFileSync(certFilePath),
+    }
+  } : undefined
+})
