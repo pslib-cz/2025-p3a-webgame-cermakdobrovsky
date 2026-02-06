f0212edb2106727585351b57e42aa8bf68e40ae4
Vít Dobrovský <dobrovsky.vit@gmail.com>
2026-02-06 16:45:56 +0100

    fix tile gaps

--- webgame.client/src/components/tiles/Tile.tsx
+++ webgame.client/src/components/tiles/Tile.tsx
@@ -1,6 +1,6 @@
 import useImage from "use-image";
 import type { MapTile } from "../../../types/mapModels";
-import { Rect, Text } from "react-konva";
+import { Image, Text } from "react-konva";
 import { useDebugMode } from "../../hooks/useDebugMode";
 
 type TileProps = {
@@ -11,28 +11,9 @@ const TileComponent: React.FC<TileProps> = ({ tile, tileSize }) => {
   const [tileImage] = useImage(tile.tile.imageUrl);
   const { debugMode } = useDebugMode();
 
-  let scale = 1;
-  let offsetX = 0;
-  let offsetY = 0;
-
-  if (tileImage) {
-    scale = Math.min(tileSize / tileImage.width, tileSize / tileImage.height);
-    offsetX = (tileSize - tileImage.width * scale) / 2;
-    offsetY = (tileSize - tileImage.height * scale) / 2;
-  }
-
   return (
     <>
-      <Rect
-        x={tile.x * tileSize}
-        y={tile.y * tileSize}
-        width={tileSize}
-        height={tileSize}
-        fillPatternImage={tileImage}
-        fillPatternScale={{ x: scale, y: scale }}
-        fillPatternOffset={{ x: -offsetX / scale, y: -offsetY / scale }}
-        fillPatternRepeat="no-repeat"
-      />
+      <Image x={tile.x * tileSize} y={tile.y * tileSize} width={tileSize + 0.5} height={tileSize + 0.5} image={tileImage} />
       {debugMode && (
         <Text
           text={`x: ${tile.x}\ny: ${tile.y}\n${tile.tile.isPlaceable}`}
