a746e1f6b273f74d8cb096de91d4c95104aeb558
Vít Dobrovský <dobrovsky.vit@gmail.com>
2026-02-09 08:43:17 +0100

    fix double click button

--- webgame.client/src/components/buttons/Button.tsx
+++ webgame.client/src/components/buttons/Button.tsx
@@ -2,18 +2,18 @@ import { type FC } from "react";
 import { useAudio } from "../../hooks/useAudio";
 
 type ButtonProps = {
-  variant?: "primary" | "secondary",
-  bgColor?: "button--primary--blue" | "button--primary--red" | "button--secondary--brown" | "button--secondary--blue",
-  imgSrc?: string,
-  smallerImg?: boolean,
-  className?: string,
-  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void,
-  children: React.ReactNode
-}
+  variant?: "primary" | "secondary";
+  bgColor?: "button--primary--blue" | "button--primary--red" | "button--secondary--brown" | "button--secondary--blue";
+  imgSrc?: string;
+  smallerImg?: boolean;
+  className?: string;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  children: React.ReactNode;
+};
 const Button: FC<ButtonProps> = ({ variant = "primary", bgColor = "button--secondary--brown", imgSrc, smallerImg = false, className, onClick, children }) => {
   //Hooks
   const { playSFX } = useAudio();
-  
+
   //Styles
   const baseStyles: string = "button";
   const variantStyles: Record<"primary" | "secondary", string> = {
@@ -31,8 +31,15 @@ const Button: FC<ButtonProps> = ({ variant = "primary", bgColor = "button--secon
   };
   return (
     <>
-      <button className={`${baseStyles} ${variantStyles[variant]} ${className}`} onClick={(e) => {handleDelayedClick(e); onClick?.(e);}}>{imgSrc && variant === "secondary" && <figure><img src={imgSrc} alt="Obrázek tlačítka"/></figure>}<span>{children}</span></button>
-      </>
-    )
-}
+      <button className={`${baseStyles} ${variantStyles[variant]} ${className}`} onClick={(e) => handleDelayedClick(e)}>
+        {imgSrc && variant === "secondary" && (
+          <figure>
+            <img src={imgSrc} alt="Obrázek tlačítka" />
+          </figure>
+        )}
+        <span>{children}</span>
+      </button>
+    </>
+  );
+};
 export default Button;
